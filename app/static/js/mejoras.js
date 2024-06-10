b7626c7ac8a2ba951444a566462fbb32e683c27e
jose <jaltamonte@gmail.com>
2024-06-10 11:35:29 -0300

    estetica y js

--- app/static/js/mejoras.js
+++ app/static/js/mejoras.js
@@ -1,22 +1,32 @@
 function imprSelec(nombre, titulo) {
-    var ficha = document.getElementById(nombre);
-    var ventimp = window.open('', 'popimpr');
-    ventimp.document.write('<html><head><title>'+titulo+'</title>');
-    ventimp.document.write(
-      '<link rel="stylesheet" href="/static/css/bootstrap/bootstrap.min.css">'
-    );
-    ventimp.document.write(
-      '<link rel="stylesheet" href="/static/css/style.css">'
-    );
-    ventimp.document.write('</head><body>');
-    ventimp.document.write(ficha.innerHTML);
-    ventimp.document.write('</body></html>');
-    ventimp.document.close();
-    
-    ventimp.print();
-    ventimp.close();
-    
+  var ficha = document.getElementById(nombre);
+  var ventimp = window.open('', 'popimpr');
+  
+  ventimp.document.write('<html><head><title>' + titulo + '</title>');
+  
+  // Agregar referencias a los estilos
+  var stylesheets = document.styleSheets;
+  for (var i = 0; i < stylesheets.length; i++) {
+      var styleSheet = stylesheets[i];
+      if (styleSheet.href) {
+          ventimp.document.write('<link rel="stylesheet" href="' + styleSheet.href + '">');
+      }
   }
+
+  ventimp.document.write('</head><body>');
+  
+  // Agregar el contenido a imprimir
+  ventimp.document.write('<div>' + ficha.innerHTML + '</div>');
+  
+  ventimp.document.write('</body></html>');
+  ventimp.document.close();
+  
+  ventimp.onload = function () {
+      ventimp.print();
+      ventimp.close();
+  };
+}
+
 
 function evitarCaracter(event, caracterProhibido) {
   var key = String.fromCharCode(event.keyCode);
@@ -33,8 +43,14 @@ window.addEventListener('load', () => {
 
   // Obtiene el valor del div contenedorQR
   const valorParaQR = contenedorQR.textContent;
-  // Crea una instancia de QRCode con el valor obtenido
-  const QR = new QRCode(contenedorQR, valorParaQR);
 
+  // Opciones para el tamaño del QR
+  const opcionesQR = {
+    text: valorParaQR,
+    width: 175, // Ancho en píxeles
+    height: 175 // Alto en píxeles
+  };
 
+  // Crea una instancia de QRCode con el valor obtenido y las opciones
+  const QR = new QRCode(contenedorQR, opcionesQR);
 });
